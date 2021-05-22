3cc0f08803a97bd95d16ef657f18c78653295093
Justenia Maire San Jose <sanjosejusteniamaire@gmail.com>
2021-05-22 15:39:08 +0800

    Change in sequence of code

--- PastelCalculator/Form1.cs
+++ PastelCalculator/Form1.cs
@@ -10,14 +10,109 @@ using System.Windows.Forms;
 
 namespace PastelCalculator
 {
-    public partial class Form1 : Form
+    public partial class Form1_calculator : Form
     {
-        public Form1()
+        public Form1_calculator()
         {
             InitializeComponent();
         }
 
-        private void one_Button_Click(object sender, EventArgs e)
+        private void Button_zero_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button_one_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button_two_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button_three_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button_four_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button_five_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button_six_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button_seven_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button_eight_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button_nine_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button_add_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button_subtract_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button_multiply_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button_divide_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button_equals_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button_delete_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button_clearEntry_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button_clear_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button_dot_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TextBox_Result_TextChanged(object sender, EventArgs e)
         {
 
         }
